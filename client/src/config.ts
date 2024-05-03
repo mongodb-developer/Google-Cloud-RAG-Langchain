4f60cb255f0690d159c966711372bbd280f9cf15
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2024-05-03 22:17:41 +0300

    Fix virtual IDE error

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,9 @@
+const backendPort = '8080';
+const backendUrl = `${location.protocol}//${location.hostname.replace('4200', `${backendPort}`)}${location.port ? `:${backendPort}` : ""}`;
+
+console.log(backendUrl);
+
 export const config = {
-    backendUrl: 'https://chat-server-rk6lsye2yq-uc.a.run.app',
+    backendUrl
 };
+
