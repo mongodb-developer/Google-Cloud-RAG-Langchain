8bed503a880ef3bd84ed39558b9007c26ef31298
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2024-06-01 17:02:04 +0300

    Delete placeholder implementation of embed-documents

--- server/src/embed-documents.ts
+++ server/src/embed-documents.ts
@@ -1,12 +1,1 @@
-import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { GoogleVertexAIEmbeddings } from "@langchain/community/embeddings/googlevertexai";
 
-import { connectToDatabase } from "./database.js";
-
-// TODO: Implement embedding documents
-
-
-process.exit(0);
